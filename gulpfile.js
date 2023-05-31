6b5db24692c409c91490725c9d1d1e872b82bc5b
andornagym <andornagy@outlook.com>
2023-05-31 22:10:12 +0300

    various additions and fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
 $scssSRC = 'src/**/*.scss';
 
 function buildStyles() {
-   return src($scssSRC).pipe(sass()).pipe(dest('dest'));
+   return (
+      src($scssSRC)
+         .pipe(sass())
+         // .pipe(purgecss({ content: ['homepage.html'] }))
+         .pipe(dest('dest'))
+   );
 }
 
 function watchTask() {
